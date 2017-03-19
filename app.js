513b523533918be1e69375d513f24fb9d1f6fcc2
Thomas Dulka <tjdulka@us.ibm.com>
2017-03-18 19:29:39 -0500

    added cfenv to packages

--- app.js
+++ app.js
@@ -12,16 +12,7 @@ var oms = require('./routes/oms');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+var cfenv = require("cfenv");
 
 var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
 var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
@@ -37,10 +28,18 @@ else{
 var dbConnection = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
 console.log("Connection String: " + dbConnection)
 
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+
 app.get('/',oms.home(ibmdb,dbConnection));
 app.get('/orders',oms.listorders(ibmdb,dbConnection));
-
-
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
